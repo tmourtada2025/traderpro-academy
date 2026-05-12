c7f2dffd87f340cf0954b28f5071d0206d10ec26
tmourtada2025 <tmourtada@gmail.com>
2026-05-12 09:10:10 +0300

    Add files via upload

--- traderpro-site/vite.config.ts
+++ traderpro-site/vite.config.ts
@@ -5,14 +5,7 @@ import path from 'node:path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'client/src'),
-    },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, 'client/src') } },
   root: path.resolve(__dirname, 'client'),
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-  },
+  build: { outDir: path.resolve(__dirname, 'dist'), emptyOutDir: true },
 });
